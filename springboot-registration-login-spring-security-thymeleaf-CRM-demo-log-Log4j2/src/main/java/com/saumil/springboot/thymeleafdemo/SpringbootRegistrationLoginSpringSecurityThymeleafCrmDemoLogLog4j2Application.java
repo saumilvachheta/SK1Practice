06b698cd7d9375710072837a6cbd31f93928d751
saumilvachheta <saumilvachheta99@gmail.com>
2021-10-18 02:05:46 +0530

    First Commit of SK Sir Project Task done by Me

---
+++ springboot-registration-login-spring-security-thymeleaf-CRM-demo-log-Log4j2/src/main/java/com/saumil/springboot/thymeleafdemo/SpringbootRegistrationLoginSpringSecurityThymeleafCrmDemoLogLog4j2Application.java
@@ -0,0 +1,13 @@
+package com.saumil.springboot.thymeleafdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRegistrationLoginSpringSecurityThymeleafCrmDemoLogLog4j2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRegistrationLoginSpringSecurityThymeleafCrmDemoLogLog4j2Application.class, args);
+	}
+
+}
---
+++ springboot-registration-login-spring-security-thymeleaf-CRM-demo-log-Log4j2/src/test/java/com/saumil/springboot/thymeleafdemo/SpringbootRegistrationLoginSpringSecurityThymeleafCrmDemoLogLog4j2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saumil.springboot.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRegistrationLoginSpringSecurityThymeleafCrmDemoLogLog4j2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
